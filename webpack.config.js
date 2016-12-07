6475fed39e9307df4443804cec623edf90d85357
j-arens <arens.joshua@sbcglobal.net>
2016-12-06 22:36:04 -0500

    basic eslint config for now

--- webpack.config.js
+++ webpack.config.js
@@ -10,12 +10,12 @@ module.exports = {
 
   module: {
     loaders: [
-      // {
-      //   test: /\.js$/,
-      //   exclude: /(node_modules)/,
-      //   loader: 'eslint-loader',
-      //   enforce: 'pre'
-      // },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        loader: 'eslint-loader',
+        enforce: 'pre'
+      },
       {
         test: /\.js$/,
         exclude: /(node_modules)/,

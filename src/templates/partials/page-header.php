ffc85650497e7602ff131f12d79a3fbc60dd254c
j-arens <arens.joshua@sbcglobal.net>
2016-12-22 17:16:57 -0500

    add conditionals

--- src/templates/partials/page-header.php
+++ src/templates/partials/page-header.php
@@ -1,3 +1,7 @@
 <div class="page-header">
-  <h1><?php ehco get_the_title() ?></h1>
+  <?php if ( is_search() ): ?>
+    <h1><?php echo 'Search: ' . get_search_query(); ?></h1>
+  <?php else: ?>
+    <h1><?php echo get_the_title(); ?></h1>
+  <?php endif ?>
 </div>

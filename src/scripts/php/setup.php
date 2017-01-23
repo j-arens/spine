268d7f8608f8f958e3e47a08e7f8d57cd2e80279
j-arens <arens.joshua@sbcglobal.net>
2017-01-23 14:42:10 -0500

    remove post and comments from admin menu by default

--- src/scripts/php/setup.php
+++ src/scripts/php/setup.php
@@ -40,6 +40,16 @@ add_action('after_setup_theme', function () {
      * @link https://developer.wordpress.org/reference/functions/add_theme_support/#html5
      */
     add_theme_support('html5', ['caption', 'gallery', 'search-form']);
+
+    /**
+    * remove unwanted menu pages in admin dashboard
+    */
+    function remove_menus() {
+    	remove_menu_page( 'edit.php' );
+    	remove_menu_page( 'edit-comments.php' );
+    }
+
+    add_action( 'admin_menu', 'remove_menus' );
 });
 
 /**

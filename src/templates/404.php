2d1c06413058ce57b04ca6fc2b001d76e77ad984
j-arens <arens.joshua@sbcglobal.net>
2017-03-01 15:47:12 -0500

    change 404 layout

--- src/templates/404.php
+++ src/templates/404.php
@@ -1,7 +1,6 @@
-<?php get_template_part('partials/page-header'); ?>
-
-<div class="alert alert-warning">
-  <?php _e('Sorry, but the page you were trying to view does not exist.', 'spine'); ?>
+<div class="404 not-found">
+  <div class="404-bg">
+    <p>Sorry, we couldn't find the page you're looking for.</p>
+    <?php get_search_form(); ?>
+  </div>
 </div>
-
-<?php get_search_form(); ?>

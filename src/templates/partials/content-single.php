2a1c63c749b98bfbbcb811e40f00bda3c0583632
j-arens <arens.joshua@sbcglobal.net>
2017-03-23 12:34:19 -0400

    add thumbnail to content single

--- src/templates/partials/content-single.php
+++ src/templates/partials/content-single.php
@@ -3,6 +3,9 @@
     <h1 class="entry-title"><?php the_title(); ?></h1>
   </header>
   <div class="entry-content">
+    <?php if (has_post_thumbnail()): ?>
+      <img src="<?php the_post_thumbnail_url('medium'); ?>" alt="post-thumbnail" class="post-thumbnail" />
+    <?php endif; ?>
     <?php the_content(); ?>
   </div>
   <footer>

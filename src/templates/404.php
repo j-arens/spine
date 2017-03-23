400f507b43c84956cd86f295eb8a3899c212119d
j-arens <arens.joshua@sbcglobal.net>
2017-03-23 12:58:10 -0400

    fix 404 decorator div

--- src/templates/404.php
+++ src/templates/404.php
@@ -1,6 +1,5 @@
 <div class="not-found">
-  <div class="not-found-decorator">
-    <p>Sorry, we couldn't find the page you're looking for.</p>
-    <?php get_search_form(); ?>
-  </div>
+  <div class="not-found-decorator"></div>
+  <p>Sorry, we couldn't find the page you're looking for.</p>
+  <?php get_search_form(); ?>
 </div>

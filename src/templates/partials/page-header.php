c5ab5d0228eb775970067539c8e13bd19a4a0fa5
j-arens <arens.joshua@sbcglobal.net>
2017-04-28 15:22:04 -0400

    add more conditional page title cases to page header

--- src/templates/partials/page-header.php
+++ src/templates/partials/page-header.php
@@ -1,7 +1,11 @@
 <div class="page-header">
   <?php if ( is_search() ): ?>
-    <h1><?php echo 'Search: ' . get_search_query(); ?></h1>
+    <h1 class="page-header__title font-yk"><?php echo 'Search: ' . get_search_query(); ?></h1>
+  <?php elseif ( is_404() ): ?>
+    <h1 class="page-header__title font-yk">Not Found</h1>
+  <?php elseif ( is_archive() ): ?>
+    <h1 class="page-header__title font-yk"><?php echo ucfirst(get_post_type()); ?> Archive</h1>
   <?php else: ?>
-    <h1><?php echo get_the_title(); ?></h1>
+    <h1 class="page-header__title font-yk"><?php echo get_the_title(); ?></h1>
   <?php endif ?>
 </div>

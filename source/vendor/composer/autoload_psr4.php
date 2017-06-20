9eb0e675d7c0d0485de001aa7b6b35eacd3aaa6f
j-arens <arens.joshua@sbcglobal.net>
2017-06-20 09:31:59 -0400

    remove carbon fields from composer, will use as a mu-plugin instead

--- source/vendor/composer/autoload_psr4.php
+++ source/vendor/composer/autoload_psr4.php
@@ -8,4 +8,5 @@ $baseDir = dirname($vendorDir);
 return array(
     'Spine\\Scripts\\' => array($baseDir . '/scripts'),
     'Spine\\Lib\\' => array($baseDir . '/lib'),
+    'Composer\\Installers\\' => array($vendorDir . '/composer/installers/src/Composer/Installers'),
 );

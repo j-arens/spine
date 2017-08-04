925c8d43a817d8044b424e0e3e07c751f0421f9d
j-arens <arens.joshua@sbcglobal.net>
2017-08-04 12:28:58 -0400

    reduce input sizes, add updated CustomFields class

--- source/scripts/php/custom-fields.php
+++ source/scripts/php/custom-fields.php
@@ -6,35 +6,95 @@ use Carbon_Fields\Container;
 use Carbon_Fields\Field;
 
 
-// hide editor on certain templates
-add_action('admin_init', function() {
-    if (!isset($_GET['post'])) return;
+add_action('admin_head', function() {
+    ?>
+        <style>
+            .field-holder label[for*="carbon-"] {
+                background: #f5f5f5;
+                display: flex !important;
+                align-items: center;
+                padding: 1em 0.5em !important;
+                margin-bottom: 0.25em;
+            }
+            .carbon-drag-handle {
+                background-color: aliceblue !important;
+            }
+            .carbon-hide-editor {
+                max-height: 0;
+                overflow: hidden;
+                position: absolute;
+            }
+        </style>
+    <?php
+});
 
-    $hideOn = [
-       'templates/front-page.php'
-   ];
-   
-   $template = get_post_meta($_GET['post'], '_wp_page_template', 1);
-    
-    if (in_array($template, $hideOn)) {
-        remove_post_type_support('page', 'editor');
+add_action('admin_enqueue_scripts', function() {
+    $screen = get_current_screen();
+    if ($screen->post_type === 'page' && $screen->base === 'post') {
+        wp_enqueue_script(
+            'carbon-fields-helper-js',
+            assetPath('scripts/js/carbonHelper.js'),
+            null,
+            filemtime(get_template_directory() . '/scripts/js/carbonHelper.js'),
+            true
+        );
     }
 });
 
-// create fields here
-add_action('carbon_register_fields', function() {
+// this class should never be instantiated, only extended by other classes creating custom fields
+// the methods within this class are meant to automate and remove some of the boilerplate with setting up carbon fields
+abstract class CustomFields {
 
-    // complex field on homepage template
-    Container::make('post_meta', 'Homepage Slider')
-        ->show_on_post_type('page')
-        ->show_on_template('templates/front-page.php')
-        ->add_fields([
-            Field::make('complex', 'Slider')
-                ->set_max(3)
-                ->add_fields('Slide', [
-                    Field::make('text', 'Title'),
-                    Field::make('image', 'Photo')
-                ])
-        ]);
+    /**
+    * Init, do some magical stuff
+    */
+    public function __construct() {
+        if (!is_admin()) 
+            return;
 
-});
+        add_action('wp_default_editor', function() {return 'tinymce';});
+        add_action('admin_init', [$this, 'hideEditor']);
+        add_action('admin_head', [$this, 'localizeTemplates']);
+        $this->registerCallbacks();
+    }
+
+    /**
+    * Register field funcs with carbon fields
+    */
+    public function registerCallbacks() {
+        foreach(get_class_methods($this) as $method) {
+            if (preg_match('/(Fields)$/', $method)) {
+                add_action('carbon_register_fields', [$this, $method]);
+            }
+        }
+    }
+
+    /**
+    * Create/add to global js var containing all the templates to hide the editor on
+    */
+    public function localizeTemplates() {
+        if (property_exists($this, 'hideEditorOn') && !empty($this->hideEditorOn)) {
+            ?>
+                <script type="text/javascript">
+                    if (!window.hasOwnProperty('carbonTemplates')) window.carbonTemplates = [];
+                    window.carbonTemplates.push(<?= json_encode($this->hideEditorOn) ?>);
+                </script>
+            <?php
+        }
+    }
+
+    /**
+    * Remove support for the mce editor if on a certain template
+    */
+    public function hideEditor() {
+        if (!isset($_GET['post']) || !property_exists($this, 'hideEditorOn') || empty($this->hideEditorOn)) {
+            return;
+        }
+
+        $template = get_post_meta($_GET['post'], '_wp_page_template', 1);
+
+        if (in_array($template, $this->hideEditorOn)) {
+            remove_post_type_support('page', 'editor');
+        }
+    }
+}

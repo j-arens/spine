72985ce6bace646df821467e782ffd8af32df4b1
j-arens <arens.joshua@sbcglobal.net>
2017-08-07 14:39:13 -0400

    add white and black to color settings, add max width to body, setup checks for homepage components

--- source/templates/front-page.php
+++ source/templates/front-page.php
@@ -2,4 +2,16 @@
 /**
  * Template Name: Homepage Template
  */
+
+    $components = [];
+
+    if (empty($components)) {
+        print '<Uh oh, there aren\'t any homepage components!</p>';
+        exit();
+    }
+
+    foreach($components as $comp) {
+        get_template_part('partials/' . $comp);
+    }
+
 ?>

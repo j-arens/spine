ba75d321bec0ccf4b19aff8ca5ce6aac050168d4
j-arens <arens.joshua@sbcglobal.net>
2017-09-27 16:57:26 -0400

    remove posts featured image from archive/search pages, add pagination styles, add back to news link to single posts

--- templates/partials/content-single.php
+++ templates/partials/content-single.php
@@ -1,4 +1,7 @@
 <article <?php post_class(); ?>>
+<?php if (in_array('single-news', get_body_class())): ?>
+  <a style="display: block; margin-bottom: 16px;" class="back-to-news" href="<?= esc_url(home_url('/')) ?>news/">&lt; All News &amp; Events</a>
+<?php endif; ?>
   <div class="entry-content">
     <?php if (has_post_thumbnail()): ?>
       <img src="<?php the_post_thumbnail_url('medium'); ?>" alt="post-thumbnail" class="post-thumbnail" />
